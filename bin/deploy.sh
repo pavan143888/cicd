cb59b31e5f9fddbae42bc83decff6853fb45bff6
pavan143888 <pavanbalineni@gmail.com>
2020-12-03 10:04:37 +0530

    added cloudformation

---
+++ bin/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+aws cloudformation package \
+  --template-file dist/template.yaml \
+  --s3-bucket "pr-cicd-pipeline-bucket" \
+  --output-template-file cloudformation/template-packaged.yaml
+
+aws cloudformation deploy \
+  --template-file cloudformation/template-packaged.yaml \
+  --stack-name "pr-cicd-pirpline-deploy-stack" \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --no-fail-on-empty-changeset \
+  --parameter-overrides \
